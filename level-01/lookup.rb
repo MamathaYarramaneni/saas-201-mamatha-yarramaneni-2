9a423ccf31a1b6baf9188aad69c0ef8488a5c0e7
MamathaYarramaneni <yarramanenimamatha2000@gmail.com>
2020-01-22 00:15:44 +0530

    my level-01 code

--- level-01/lookup.rb
+++ level-01/lookup.rb
@@ -1,52 +1,47 @@
 def get_command_line_argument
-
   if ARGV.empty?
     puts "Usage: ruby lookup.rb <domain>"
     exit
   end
   ARGV.first
 end
+
 domain = get_command_line_argument
+
 dns_raw = File.readlines("zone.txt")
 
 def parse_dns(dns_raw)
-    arr1=[]
-    dns_raw.each{|x|  arr1.push(x.split(",").map(&:strip))}
-    hash={"A"=>{},"CNAME"=>{}}
-    arr1.each do |x|
-        if x[0]=="A"
-            hash["A"][x[1]]= x[2]
-        else
-            hash["CNAME"][x[1]] = x[2]
-        end
+  split_input_array = []
+  dns_raw.each { |dns_value| split_input_array.push(dns_value.split(",").map(&:strip)) }
+  hash = { "A" => {}, "CNAME" => {} }
+  split_input_array.each do |dns_value|
+    cname_var = dns_value[0]
+    domain_var = dns_value[1]
+    ip_address_or_alias = dns_value[2]
+
+    if cname_var == "A"
+      hash["A"][domain_var] = ip_address_or_alias
+    else
+      hash["CNAME"][domain_var] = ip_address_or_alias
     end
-    return hash
+  end
+  return hash
 end
 
 def resolve(dns_records, lookup_chain, domain)
-    dns_records.each do |key, value|
-        if key=="A"
-            value.each do |k,v|
-                if k==domain
-                    lookup_chain.push(v.to_s)
-                end
-            end
-        end
-        if key=="CNAME"
-            value.each do |k,v|
-                if k==domain
-                    lookup_chain.push(v.to_s)
-                    resolve(dns_records, lookup_chain,v)
-                end
-            end
-        end
-    end
-    if lookup_chain.length==1
-        lookup_chain=[]
-        lookup_chain.push("Error: record not found for #{domain}")
-    end
-    return lookup_chain
-
+  ip_address = dns_records["A"][domain]
+  cname_alias = dns_records["CNAME"][domain]
+  if ip_address
+    lookup_chain.push(ip_address)
+  elsif cname_alias
+    lookup_chain.push(cname_alias)
+    resolve(dns_records, lookup_chain, cname_alias)
+  end
+  if lookup_chain.length == 1
+    lookup_chain = []
+    lookup_chain.push("Error: record not found for #{domain}")
+  end
+  return lookup_chain
 end
 
 dns_records = parse_dns(dns_raw)
