37230a33543d3aaed14d919e239fb6c1f0e8bfb1
MamathaYarramaneni <yarramanenimamatha2000@gmail.com>
2020-02-12 21:30:34 +0530

    level-04 code

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -15,70 +15,56 @@ class Todo < ActiveRecord::Base
     Date.today < due_date
   end
 
-  def idfind
-    id
-  end
-
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
     display_date = due_today? ? nil : due_date
 
     if display_status
-      return "#{idfind} #{display_status} #{todo_text} #{display_date}"
+      return "#{id} #{display_status} #{todo_text} #{display_date}"
     else
       return nil
     end
   end
 
   def self.to_displayable_string
-    (all.map { |todo|
-      if (todo.findid)
-        puts todo.to_displayable_string
-      else
-        puts "no row found with id #{findid}"
-      end
-    })
+    all.map { |todo|
+      todo.to_displayable_string
+    }
   end
 
   def self.show_list
     puts "My Todo-list\n\n"
 
     puts "\nOverdue\n"
-    (all.map { |todo|
-      if (todo.overdue?)
-        puts todo.to_displayable_string
-      end
+    (all.where("due_date < ?", Date.today).map { |todo|
+      puts todo.to_displayable_string
     })
     puts "\n\n"
 
     puts "Due Today\n"
-    (all.map { |todo|
-      if (todo.due_today?)
-        puts todo.to_displayable_string
-      end
+    (all.where("due_date = ?", Date.today).map { |todo|
+      puts todo.to_displayable_string
     })
     puts "\n\n"
 
     puts "Due Later\n"
-    (all.map { |todo|
-      if (todo.due_later?)
-        puts todo.to_displayable_string
-      end
+    (all.where("due_date > ?", Date.today).map { |todo|
+      puts todo.to_displayable_string
     })
     puts "\n\n"
   end
 
   def self.add_task(hash)
-    todotextvariable = hash[:todo_text]
-    dueindaysvariable = hash[:due_in_days]
-    completedvariable = (dueindaysvariable > 0) ? false : true
+    dueindaysvar = hash[:due_in_days].to_i
+    completedvar = (dueindaysvar > 0) ? false : true
 
-    Todo.create!(todo_text: todotextvariable.to_s, due_date: Date.today + dueindaysvariable.to_i, completed: completedvariable)
+    Todo.create!(todo_text: hash[:todo_text], due_date: Date.today + dueindaysvar, completed: completedvar)
   end
 
   def self.mark_as_complete!(todo_id)
-    idvariable = Todo.find(todo_id)
-    idvariable.save
-    idvariable
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
